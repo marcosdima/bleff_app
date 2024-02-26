f8e756d2f05537d64d9bdeebeea6c76227c2c214
marcosdima <marcosdimatteo.wgr@gmail.com>
2024-02-26 16:23:57 -0300

    New: UserConnected Displays the number of users connected

---
+++ frontend/src/components/simple/UserConnected.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="users-connected">
+    <div v-for="index in userMax" :key="index">
+      <font-awesome-icon
+        v-if="index <= userTotal"
+        icon="user"
+        class="element"
+      />
+      <font-awesome-icon
+        v-else
+        icon="user"
+        style="color: darkgrey"
+        class="element"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+export default {
+  components: {
+    FontAwesomeIcon,
+  },
+  props: {
+    userMax: {
+      type: Number,
+      required: true,
+    },
+    userTotal: {
+      type: Number,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.users-connected {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+.element {
+  margin: 2px;
+}
+</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,4 +3,13 @@ import App from "./App.vue";
 import router from "./router";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-createApp(App).use(router).mount("#app");
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faUser);
+
+const app = createApp(App);
+app.use(router);
+app.component("font-awesome-icon", FontAwesomeIcon);
+app.mount("#app");
